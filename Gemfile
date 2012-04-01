0a9ad289bd679a3198e6a6f2106e596548430993
Ryan Bigg <radarlistener@gmail.com>
2012-03-31 22:32:33 -0500

    Setup gem dependencies and ran rspec generator

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test, :development do
+  gem 'rspec-rails', '~> 2.9'
+end
+
+group :test do
+  gem 'capybara', '1.1.2'
+  gem 'database_cleaner', '0.7.2'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '8db41dab7f82f43583332371f3d292fb782b7d709c382f31451f28315b2559843dccefecdf0edf6c0aff43814d5a338f1bd7c04f6dfd4f23fe761bc5be5ba138'
