5fe3cfa539d8243ae0501e7c0308aa877e591750
Ryan Bigg <radarlistener@gmail.com>
2012-05-20 15:32:11 +1000

    Don't use concat on authorized? helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
   end
 
   def authorized?(permission, thing, &block)
-    concat(block.call) if can?(permission.to_sym, thing) ||
-                          current_user.try(:admin?)
+    block.call if can?(permission.to_sym, thing) ||
+                  current_user.try(:admin?)
   end
 end
