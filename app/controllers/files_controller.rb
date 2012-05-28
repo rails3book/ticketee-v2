30fb9d4a5e2861da1bd371be2a1d56b1e53a40e0
Ryan Bigg <radarlistener@gmail.com>
2012-05-29 08:04:06 +1000

    Provide an 'Add another file' link that uses Javascript so that users can upload more than one file

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,5 +1,15 @@
 class FilesController < ApplicationController
   before_filter :authenticate_user!
+
+  def new
+    @ticket = Ticket.new
+    asset = @ticket.assets.build
+    render :partial => "files/form",
+           :locals => { :number => params[:number].to_i,
+                        :asset => asset }
+
+  end
+
   def show
     asset = Asset.find(params[:id])
     if can?(:view, asset.ticket.project)
--- spec/integration/creating_tickets_spec.rb
+++ spec/integration/creating_tickets_spec.rb
@@ -6,6 +6,7 @@ feature "Creating Tickets" do
     user = Factory(:confirmed_user, :email => "ticketee@example.com")
     define_permission!(user, "view", project)
     define_permission!(user, "create tickets", project)
+    puts "Signing in as..."
     sign_in_as!(user)
 
     visit '/'
@@ -40,18 +41,17 @@ feature "Creating Tickets" do
     page.should have_content("Description is too short")
   end
 
-  scenario "Creating a ticket with an attachment" do
+  scenario "Creating a ticket with an attachment", :js => true do
     fill_in "Title", :with => "Add documentation for blink tag"
-    fill_in "Description", :with => "The blink tag has an undocumented speed attribute"
-    attach_file "File #1", "spec/fixtures/speed.txt"
-    attach_file "File #2", "spec/fixtures/spin.txt"
-    attach_file "File #3", "spec/fixtures/gradient.txt"
+    fill_in "Description", :with => "Blink tag's undocumented speed attribute"
+    attach_file "File #1", File.expand_path("spec/fixtures/speed.txt")
+    click_link "Add another file"
+    attach_file "File #2", File.expand_path("spec/fixtures/spin.txt")
     click_button "Create Ticket"
     page.should have_content("Ticket has been created.")
     within("#ticket .assets") do
       page.should have_content("speed.txt")
       page.should have_content("spin.txt")
-      page.should have_content("gradient.txt")
     end
   end
 end
