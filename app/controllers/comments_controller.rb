ca2482e24604305b8d6ae2a3d1ebeac234bfc033
Ryan Bigg <radarlistener@gmail.com>
2012-06-20 07:33:41 +1000

    Protect state_id from users who do not have permission to change it

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,26 @@
 class CommentsController < ApplicationController
-   before_filter :authenticate_user!
-   before_filter :find_ticket
+  before_filter :authenticate_user!
+  before_filter :find_ticket
 
-   def create
-     @comment = @ticket.comments.build(params[:comment])
-     @comment.user = current_user
-     if @comment.save
-       flash[:notice] = "Comment has been created."
-       redirect_to [@ticket.project, @ticket]
-     else
-       @states = State.all
-       flash[:alert] = "Comment has not been created."
-       render :template => "tickets/show"
-     end
-   end
+  def create
+    if cannot?(:"change states", @ticket.project)
+      params[:comment].delete(:state_id)
+    end
+    @comment = @ticket.comments.build(params[:comment])
+    @comment.user = current_user
+    if @comment.save
+      flash[:notice] = "Comment has been created."
+      redirect_to [@ticket.project, @ticket]
+    else
+      @states = State.all
+      flash[:alert] = "Comment has not been created."
+      render :template => "tickets/show"
+    end
+  end
 
-   private
+  private
 
-     def find_ticket
-       @ticket = Ticket.find(params[:ticket_id])
-     end
- end
+  def find_ticket
+    @ticket = Ticket.find(params[:ticket_id])
+  end
+end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe CommentsController do
+  let(:user) { Factory(:confirmed_user) }
+  let(:project) { Project.create!(:name => "Ticketee") }
+
+  let(:ticket) do
+    ticket = project.tickets.build(:title => "State transitions",
+                                   :description => "Can't be hacked.")
+    ticket.user = user
+    ticket.save
+    ticket
+  end
+
+  let(:state) { State.create!(:name => "New") }
+
+  context "a user without permission to set state" do
+    before do
+      sign_in(:user, user)
+    end
+
+    it "cannot transition a state by passing through state_id" do
+      post :create, { :comment => { :text => "Hacked!",
+        :state_id => state.id },
+        :ticket_id => ticket.id }
+      ticket.reload
+      ticket.state.should eql(nil)
+    end
+  end
 
 end
