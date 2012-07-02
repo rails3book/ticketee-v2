0a54129f48c93c05f4047bd7bec88d514c3b0600
Ryan Bigg <radarlistener@gmail.com>
2012-07-02 17:54:24 +1000

    Improve styling

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :description
 
   validates :name, :presence => true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120610043514) do
+ActiveRecord::Schema.define(:version => 20120630063001) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -44,8 +44,9 @@ ActiveRecord::Schema.define(:version => 20120610043514) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.text     "description"
   end
 
   create_table "states", :force => true do |t|
