36f68a4ddb00eb4f716940897eed7da1257a6f6f
Ryan Bigg <radarlistener@gmail.com>
2012-07-09 17:14:28 +1000

    Users may now search for tickets by state or tag

--- spec/integration/searching_spec.rb
+++ spec/integration/searching_spec.rb
@@ -4,19 +4,23 @@ feature "Searching" do
   let!(:user) { Factory(:confirmed_user) }
   let!(:project) { Factory(:project) }
   let!(:ticket_1) do
+    state = State.create(:name => "Open")
     Factory(:ticket,
             :title => "Create projects",
             :project => project,
             :user => user,
-            :tag_names => "iteration_1")
+            :tag_names => "iteration_1",
+            :state => state)
   end
 
   let!(:ticket_2) do
+    state = State.create(:name => "Closed")
     Factory(:ticket,
             :title => "Create users",
             :project => project,
             :user => user,
-            :tag_names => "iteration_2")
+            :tag_names => "iteration_2",
+            :state => state)
   end
 
   before do
@@ -30,7 +34,18 @@ feature "Searching" do
   scenario "Finding by tag" do
     fill_in "Search", :with => "tag:iteration_1"
     click_button "Search"
-    page.should have_content("Create projects")
-    page.should_not have_content("Create users")
+    within("#tickets") do
+      page.should have_content("Create projects")
+      page.should_not have_content("Create users")
+    end
+  end
+
+  scenario "Finding by state" do
+    fill_in "Search", :with => "state:Open"
+    click_button "Search"
+    within("#tickets") do
+      page.should have_content("Create projects")
+      page.should_not have_content("Create users")
+    end
   end
 end
