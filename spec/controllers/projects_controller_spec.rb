b67926f088b5dfa37cb2b9c04f0c72074d914cd3
Ryan Bigg <radarlistener@gmail.com>
2012-08-07 07:38:58 +1000

    Use symbols for multi-action tests in ProjectsController

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -10,15 +10,14 @@ describe ProjectsController do
       sign_in(:user, user)
     end
 
-
-    { "new" => "get",
-      "create" => "post",
-      "edit" => "get",
-      "update" => "put",
-      "destroy" => "delete" }.each do |action, method|
+    { :new => :get,
+      :create => :post,
+      :edit => :get,
+      :update => :put,
+      :destroy => :delete }.each do |action, method|
         it "cannot access the #{action} action" do
           sign_in(:user, user)
-          send(method, action.dup, :id => project.id)
+          send(method, action, :id => project.id)
           response.should redirect_to(root_path)
           flash[:alert].should eql("You must be an admin to do that.")
         end
